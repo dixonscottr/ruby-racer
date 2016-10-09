2db51b82e2c23c2acb6ae4d0602535d19df14def
Scott Dixon <dixon.scottr@gmail.com>
2016-10-09 00:43:13 -0400

    Clean up code, delete comments

--- ruby_racer.rb
+++ ruby_racer.rb
@@ -11,15 +11,11 @@ class RubyRacer
   end
 
   def record_player_symbols(array_of_symbols)
-
     array_of_symbols.each do |symbol|
       @player_locations[symbol] = 0
     end
-
   end
 
-  # Returns +true+ if one of the players has reached
-  # the finish line, +false+ otherwise
   def finished?
     finished = player_locations.values.select do |location|
       location >= (length - 1)
@@ -27,60 +23,24 @@ class RubyRacer
     finished.empty? ? false : true
   end
 
-  # Returns the winner if there is one, +nil+ otherwise
   def winner
-
-    # winner = nil
-    # front = 0
-    # player_locations.each do |player, location|
-    #   if location > front
-    #     front = location
-    #     winner = player
-    #   end
-    # end
-    # winner
-
     winner =  player_locations.key(@player_locations.values.max)
-
     winning_location = player_locations[winner]
-
-    # ties = player_locations.values.select {|location| location == winning_location}
-
-    # ties.empty? ? nil : ties
-
     player_locations.values.count(winning_location) > 1 ? nil : winner
-
-
   end
 
-  # Rolls the die and advances +player+ accordingly
   def advance_players
     @player_locations.update(@player_locations) do |player,location|
       location += die.roll
       location > 29 ? location = 29 : location
     end
-
-
   end
 
-  # Returns the current state of the game as a string
-  # that can be printed on the command line.
-  # The board should have the same dimensions each time.
   def board_visualization
     board = Array.new(num_of_players) { Array.new(@length, " ") }
-    # board[0][@player_one_location] = players[0]
-    # board[1][@player_two_location] = players[1]
-    # board.each do |row|
-    #   puts row.join("|") + " Finish"
-    # end
     player_locations.each_with_index do |(player, location), idx|
-      # p player
-      # p board[idx]
-      # p board[idx][location]
       board[idx][location] = player
     end
-    board.map do |row|
-      row.join("|") + " Finish"
-    end
+    board.map { |row| row.join("|") + " Finish" }
   end
 end
--- runner.rb
+++ runner.rb
@@ -2,8 +2,6 @@ require_relative 'die'
 require_relative 'ruby_racer'
 require_relative 'reset_screen'
 
-
-# players = [:a, :b]
 die = Die.new
 puts "Welcome to Ruby Racer"
 puts "How many racers would like to compete?"
@@ -23,37 +21,21 @@ end
 
 game.record_player_symbols(player_symbols)
 
-# game = RubyRacer.new(players, die)
-
-# Clear the screen and print the board
-# with players in their starting positions.
-# Then pause, so users can see the starting board.
 reset_screen
 puts game.board_visualization
 sleep(1)
 
-# Play the game.
 until game.finished?
-  # Do this each round until the game is finished.
 
-# Move each player forward.
-  # game.players.each do |player|
-  #   game.advance_player(player)
-  # end
   game.advance_players
 
-  # Now that each player has moved,
-  # reprint the board with the new player positions
-  # and pause so users can see the updated board.
   reset_screen
   puts game.board_visualization
   sleep(0.2)
 end
 
-# Once the game is finished, report the winner.
 if game.winner
   puts "Player '#{game.winner}' wins!"
-  # p game.winner
 else
   puts "It's a tie!"
 end
