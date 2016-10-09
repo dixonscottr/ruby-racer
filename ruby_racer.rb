1919b295d61597e3673e94d0c60049849347b134
Scott Dixon <dixon.scottr@gmail.com>
2016-10-09 00:20:36 -0400

    Add functionality for multiple players

--- ruby_racer.rb
+++ ruby_racer.rb
@@ -1,53 +1,84 @@
 require 'pry'
 class RubyRacer
-  attr_reader :players, :die, :length
-  attr_accessor :player_one_location, :player_two_location
+  attr_reader :num_of_players, :die, :length
+  attr_accessor :player_locations
 
-  def initialize(players, die, length = 30)
-    @players = players
+  def initialize(num_of_players, die, length = 30)
     @die = die
     @length = length
-    @player_one_location = 0
-    @player_two_location = 0
+    @num_of_players = num_of_players
+    @player_locations = {}
+  end
+
+  def record_player_symbols(array_of_symbols)
+
+    array_of_symbols.each do |symbol|
+      @player_locations[symbol] = 0
+    end
+
   end
 
   # Returns +true+ if one of the players has reached
   # the finish line, +false+ otherwise
   def finished?
-    if player_one_location >= (length-1) || player_two_location >= (length-1)
-      true
-    else
-      false
+    finished = player_locations.values.select do |location|
+      location >= (length - 1)
     end
+    finished.empty? ? false : true
   end
 
   # Returns the winner if there is one, +nil+ otherwise
   def winner
-    if player_one_location > player_two_location
-      players[0]
-    elsif player_two_location > player_one_location
-      players[1]
-    else
-      nil
-    end
+
+    # winner = nil
+    # front = 0
+    # player_locations.each do |player, location|
+    #   if location > front
+    #     front = location
+    #     winner = player
+    #   end
+    # end
+    # winner
+
+    winner =  player_locations.key(@player_locations.values.max)
+
+    winning_location = player_locations[winner]
+
+    # ties = player_locations.values.select {|location| location == winning_location}
+
+    # ties.empty? ? nil : ties
+
+    player_locations.values.count(winning_location) > 1 ? nil : winner
+
+
   end
 
   # Rolls the die and advances +player+ accordingly
-  def advance_player(player)
-    # binding.pry
-    @player_one_location += die.roll
-    @player_two_location += die.roll
-    @player_one_location = 29 if player_one_location > 29
-    @player_two_location = 29 if player_two_location > 29
+  def advance_players
+    @player_locations.update(@player_locations) do |player,location|
+      location += die.roll
+      location > 29 ? location = 29 : location
+    end
+
+
   end
 
   # Returns the current state of the game as a string
   # that can be printed on the command line.
   # The board should have the same dimensions each time.
   def board_visualization
-    board = Array.new(@players.length) { Array.new(@length, " ") }
-    board[0][@player_one_location] = players[0]
-    board[1][@player_two_location] = players[1]
+    board = Array.new(num_of_players) { Array.new(@length, " ") }
+    # board[0][@player_one_location] = players[0]
+    # board[1][@player_two_location] = players[1]
+    # board.each do |row|
+    #   puts row.join("|") + " Finish"
+    # end
+    player_locations.each_with_index do |(player, location), idx|
+      # p player
+      # p board[idx]
+      # p board[idx][location]
+      board[idx][location] = player
+    end
     board.map do |row|
       row.join("|") + " Finish"
     end
--- runner.rb
+++ runner.rb
@@ -3,10 +3,27 @@ require_relative 'ruby_racer'
 require_relative 'reset_screen'
 
 
-players = [:a, :b]
+# players = [:a, :b]
 die = Die.new
+puts "Welcome to Ruby Racer"
+puts "How many racers would like to compete?"
 
-game = RubyRacer.new(players, die)
+num_of_players = gets.to_i
+
+game = RubyRacer.new(num_of_players, die, 30)
+num = 1
+player_symbols= []
+
+num_of_players.times do 
+  puts "Please enter the symbol for player #{num}"
+  player_num = gets.chomp.to_sym
+  player_symbols << player_num
+  num += 1
+end
+
+game.record_player_symbols(player_symbols)
+
+# game = RubyRacer.new(players, die)
 
 # Clear the screen and print the board
 # with players in their starting positions.
@@ -20,9 +37,10 @@ until game.finished?
   # Do this each round until the game is finished.
 
 # Move each player forward.
-  game.players.each do |player|
-    game.advance_player(player)
-  end
+  # game.players.each do |player|
+  #   game.advance_player(player)
+  # end
+  game.advance_players
 
   # Now that each player has moved,
   # reprint the board with the new player positions
@@ -35,6 +53,7 @@ end
 # Once the game is finished, report the winner.
 if game.winner
   puts "Player '#{game.winner}' wins!"
+  # p game.winner
 else
   puts "It's a tie!"
 end
